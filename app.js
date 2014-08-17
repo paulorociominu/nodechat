65dd19e35c8b702b390333e63f8dcac3decc2767
cosmenavarro <cosmenavarro@gmail.com>
2014-08-17 03:41:45 -0300

    Chat base implementation

--- app.js
+++ app.js
@@ -1,21 +1,70 @@
 //nodechat
 //A simple chat app using express, angular and async.
 
-var path = require('path');
 var async = require('async');
+var path = require('path');
 var express = require('express');
-
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+//Creates a complete router for static files on a folder
+app.use(express.static(path.resolve(__dirname, 'client')));
 
 //Collections
 var messages = [];
 var sockets = [];
 
-//Creates a complete router for static files on a folder
-app.use(express.static(path.resolve(__dirname, 'client')));
+io.on('connection', function (socket) {
+  console.log('new user conected...');
 
-app.listen(3000, function() {
+  //Add socket to collection
+  sockets.push(socket);
+
+  //Emit all previous messages to the socket
+  messages.forEach(function (data) {
+    socket.emit('message', data);
+  });
+
+  //Listen to new message event from socket
+  socket.on('message', function (msg) {
+    var text = String(msg || '');
+
+    if (!text)
+      return;
+
+    var data = {
+      name: socket.name,
+      text: text
+    };
+
+    io.emit('message', data);
+    messages.push(data);
+  });
+
+  //Listen to user name change
+  socket.on('identify', function (name) {
+    socket.name = name ? name : 'Anonymous';
+    updateRoster();
+  });
+
+  //Listen to user disconnect
+  socket.on('disconnect', function(){
+    console.log('user disconnected...');
+    var index = sockets.indexOf(socket);
+    sockets.splice(index, 1);
+    updateRoster();
+  });
+});
+
+function updateRoster() {
+  names = [];
+  sockets.forEach(function(socket){
+    names.push(socket.name);
+  });
+  io.emit('roster', names);
+}
+
+http.listen(3000, function() {
   console.log('Listening at 3000...')
 });
